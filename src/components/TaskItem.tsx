bf3cef9e7592a6f1a87212155c30ee8f5b8e503c
Orgris <vjmax793@gmail.com>
2024-09-03 17:35:12 +0300

    feat: add task sub-menu

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,20 +1,70 @@
-import { Button, Checkbox, HStack, IconButton } from "@chakra-ui/react";
+import {
+  Button,
+  Checkbox,
+  HStack,
+  IconButton,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
 import AlertDialogComponent from "./AlertDialogComponent";
-import { ListType } from '@/app/page';
-import { useState } from 'react';
-import { CheckIcon } from '@chakra-ui/icons';
+import { ListType } from "@/app/page";
+import { useState } from "react";
+import { CheckIcon, EditIcon, HamburgerIcon } from "@chakra-ui/icons";
+import ChangeDateModal from "./ChangeDateModal";
+import TaskIcon from "./TaskIcon";
 
 type Props = {
-  task: ListType
-  handleChangeChecked: (id: number) => void
-  deleteTask: (id: number) => void
-}
+  task: ListType;
+  handleChangeChecked: (id: number) => void;
+  deleteTask: (id: number) => void;
+};
 
 const TaskItem = ({ task, handleChangeChecked, deleteTask }: Props) => {
-  const [isActive, setIsActive] = useState(task.isCompleted)
+  const [isActive, setIsActive] = useState(task.isCompleted);
   const onClickDelete = () => {
-    deleteTask(task.id)
-  }
+    deleteTask(task.id);
+  };
+
+  // const [taskTade, setTaskTade] = useState<Date>()
+  // const date = new Date(task.id);
+  // setTaskTade(date)
+
+  //-----------------------
+
+  // function TaskDate() {
+  //   const timestamp = task.id;
+  //   const date = new Date(timestamp);
+
+  //   const [currentTime, setCurrentTime] = useState(date);
+  //   setCurrentTime(date);
+
+  //   console.log(date);
+  //    return (
+  //        <Text>{currentTime}</Text>
+  //   );
+  // }
+
+  //-----------------------
+
+  // function formatDateTime = ({ task.id }: Props) => {
+  //   // Создаем объект Date из таймстампа
+  //   const date = new Date(task.id);
+
+  //   // Форматируем дату и время
+  //   const year = date.getFullYear();
+  //   const month = (`0${date.getMonth() + 1}`).slice(-2);
+  //   const day = (`0${date.getDate()}`).slice(-2);
+  //   const hour = (`0${date.getHours()}`).slice(-2);
+  //   const minute = (`0${date.getMinutes()}`).slice(-2);
+  //   const second = (`0${date.getSeconds()}`).slice(-2);
+
+  //   // Возвращаем отформатированную строку
+  //   return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+  // }
 
   return (
     <HStack
@@ -32,23 +82,78 @@ const TaskItem = ({ task, handleChangeChecked, deleteTask }: Props) => {
       <Button
         variant="unstyled"
         border="1px solid"
+        borderRadius={"full"}
+        height={5}
+        width={5}
+        minH={0}
+        minW={0}
         borderColor="PRIMARY_PURPLE"
-        onClick={() => {handleChangeChecked(task.id)
-          setIsActive(!isActive)
+        textAlign={"center"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        onClick={() => {
+          handleChangeChecked(task.id);
+          setIsActive(!isActive);
         }}
       >
-        {isActive ? <CheckIcon /> : null}
+        {isActive ? <CheckIcon color={"PRIMARY_PURPLE"}/> : null}
       </Button>
-
-      {/* <Checkbox checked={task.isCompleted}
-        onChange={() => handleChangeChecked(task.id)}
-      /> */}
-
       <p style={{ flex: "1" }}>{task.title}</p>
+      <p>{task.id}</p> //TODO Сделать дату
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Options"
+          icon={<TaskIcon />}
+          variant="outline"
+          bg={"none"}
+          minW={0}
+          minH={0}
+          h={5}
+          border={"none"}
+          _hover={{
+            bg: "none",
+          }}
+          _expanded={{
+            bg: "none",
+          }}
+        />
+        <MenuList
+          display="flex"
+          flexDirection="row"
+          gap={2}
+          width={"fit-content"}
+          height={"fit-content"}
+          minH={0}
+          minW={0}
+          paddingY={1}
+          paddingX={2}
+          borderRadius={10}
+          borderWidth={0.25}
+          borderColor={"PRIMARY_PURPLE"}
+        >
+          <MenuItem
+            width={"auto"}
+            padding={0}
+            _hover={{
+              bg: "none",
+            }}
+          >
+            <ChangeDateModal />
+          </MenuItem>
 
-      <p>Date</p>      //TODO Сделать дату
-
-      <AlertDialogComponent onClickDelete={onClickDelete} /> //TODO добавить удаление в компонент
+          <MenuItem
+            width={"auto"}
+            padding={0}
+            _hover={{
+              bg: "none",
+            }}
+          >
+            <AlertDialogComponent onClickDelete={onClickDelete} />
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </HStack>
   );
 };
